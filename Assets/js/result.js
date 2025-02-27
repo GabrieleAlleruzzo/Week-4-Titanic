62829785b7d027631e4debd5781a5926ee5e4848
MicheleFamoso <michelefamoso@gmail.com>
2025-02-27 12:37:50 +0100

    risolto

--- Assets/js/result.js
+++ Assets/js/result.js
@@ -24,3 +24,75 @@ if (ansTrueAvanza !== null) {
   percLeft.innerText = "0%";
   percRight.innerText = "100%";
 }
+let g = ansTrueAvanza * 10;
+let d = (10 - ansTrueAvanza) * 10;
+var xValues = ["Right", "Wrong"];
+var yValues = [d, g];
+var barColors = ["#d20094", "#00ffff"];
+
+var chartCanvas = document.getElementById("myChart");
+chartCanvas.width = 600;
+chartCanvas.height = 600;
+chartCanvas.style.filter = "drop-shadow(0px 0px 20px rgba(0, 2, 46, 0.8))";
+
+new Chart("myChart", {
+  type: "doughnut",
+  data: {
+    datasets: [
+      {
+        backgroundColor: barColors,
+        data: yValues,
+        borderWidth: 0,
+      },
+    ],
+  },
+  options: {
+    cutoutPercentage: 70,
+    title: {
+      display: false,
+      text: "Results",
+    },
+  },
+});
+
+let bocciato = document.querySelector("strong#Congratulations");
+let azzurro = document.getElementById("azzurro");
+let descrizione = document.querySelector("p#descrizione");
+
+let esame = function (ans) {
+  ans = ansTrueAvanza;
+  if (ans < 6) {
+    azzurro.innerText = `You not passed the exam`;
+    bocciato.innerText = "Failed!";
+
+    descrizione.innerText = "Retry the exam for the qualification";
+  } else {
+    bocciato.innerText = "Congratulations!";
+    azzurro.innerText = "You passed the exam.";
+    descrizione.innerText = `We'll send you the certificate in a few minutes. check your email
+  (including promotions/spam folder)`;
+  }
+};
+esame();
+
+/*if (ansTrueAvanza < 6) {
+  bocciato.innerText = "Failed!";
+} else {
+  bocciato.innerText = "Congratulations!";
+  console.log("ciao");
+}
+
+if (ansTrueAvanza < 6) {
+  azzurro.innnerText = "You not passed the exam.";console.log("ss");
+} else {
+  azzurro.innerText = "You passed the exam.";
+  
+}
+
+if (ansTrueAvanza < 6) {
+  descrizione.innerText = "Retry the exam for the qualification";
+} else {
+  descrizione.innerText = `We'll send you the certificate in a few minutes. check your email
+  (including promotions/spam folder)`;
+  console.log("ee");
+}*/
